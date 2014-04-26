0623a8e3a9b226c1d7fb1cbe5dbffba0eae7afdf
Rock <amrendra93@gmail.com>
2014-04-26 00:21:07 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -34,7 +34,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## cacheSolve calculates the inverse of the input Matrix created 
-## with the above function and returns the nverse of the input matrix. But first checks
+## with the above function and returns the inverse of the input matrix. But first checks
 ## to see if the inverse has already been calculated and data has not changed, if yes, 
 ## then skips the computation and returns inverse matrix from cache saved by first fucntion.
 
